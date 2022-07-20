7677341699a30229a71de27172fecc16b21e8929
karishmask46 <karishmask.15999@gmail.com>
2022-07-20 15:14:50 +0530

    find the max float number

--- Day13_GenericsProblem/Program.cs
+++ Day13_GenericsProblem/Program.cs
@@ -12,6 +12,17 @@ public class program
         UC1_FindMaximumInteger findmax = new UC1_FindMaximumInteger();
         if (findmax.Getmaximum(first_number, second_number, third_number) != 0)
             Console.WriteLine("{0} is Maximum out of {1} ,{2}, {3}", findmax.Getmaximum(first_number, second_number, third_number), first_number, second_number, third_number);
+        Console.WriteLine("Enter the first number");
+        Double firstValue = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        Double secondValue = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the third number");
+        Double thirdValue = Convert.ToDouble(Console.ReadLine());
+        UC1_FindMaximumInteger maxFloat = new UC1_FindMaximumInteger();
+        if (maxFloat.MaximumFloatNumber(firstValue, secondValue, thirdValue) != 0)
+
+            Console.WriteLine("{0} is Maximum out of {1} ,{2}, {3}", maxFloat.MaximumFloatNumber(firstValue, secondValue, thirdValue), firstValue, secondValue, thirdValue);
+
     }
 }
 
