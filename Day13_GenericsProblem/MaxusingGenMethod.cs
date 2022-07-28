c724d51fe3b785307659bf5bf39b338d74137eff
karishmask46 <karishmask.15999@gmail.com>
2022-07-28 15:23:51 +0530

    Extend max method using Generic Method

---
+++ Day13_GenericsProblem/MaxusingGenMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13_GenericsProblem
+{
+    public class MaxNumberCheck<T>
+    {
+        public T[] array;
+        public MaxNumberCheck(T[] array)
+        {
+            this.array = array;
+        }
+        public T[] Sort(T[] inputArray)
+        {
+            Array.Sort(inputArray);
+            return inputArray;
+        }
+        public T PrintMax()
+        {
+            var sorted_values = Sort(this.array);
+            return sorted_values[sorted_values.Length - 1];
+        }
+        public void testMaximum()
+        {
+            var max = PrintMax();
+            Console.WriteLine(max);
+        }
+    }
+}
+
+       
--- Day13_GenericsProblem/Program.cs
+++ Day13_GenericsProblem/Program.cs
@@ -3,15 +3,16 @@ public class program
 {
     public static void Main(string[] args)
     {
-
-
         int[] intArray = { 34, 40, 89, 80, 86, 34,};
         double[] doubleArray = { 55.76f, 67.02f, 78.12f,76.8f,45.3f, 64.8f};
         string[] stringArray = { "Driver", "Pilot", "Engineer", "Doctor", "IASOfficer","RevenueOfficer" };
-        Sortmethod.FindMax<int>(intArray);
-        Sortmethod.FindMax<double>(doubleArray);
-        Sortmethod.FindMax<string>(stringArray);
+        MaxNumberCheck<int> integer = new MaxNumberCheck<int>(intArray);
+        MaxNumberCheck<double> doublenum = new MaxNumberCheck<double>(doubleArray);
+        MaxNumberCheck<string> stringword = new MaxNumberCheck<string>(stringArray);
 
+        integer.testMaximum();
+        doublenum.testMaximum();
+        stringword.testMaximum();
     }
 }
 
